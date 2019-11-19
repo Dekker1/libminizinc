428b1fa9a1bd70988628b283293166d7e4dce11d
Jip J. Dekker <jip@dekker.one>
2019-11-19 14:49:54 +1100

    The corrected fix for the <= operator

--- lib/codegen.cpp
+++ lib/codegen.cpp
@@ -234,7 +234,7 @@ void call_binop(CodeGen& cg, CG_Builder& frag, Mode ctx, BinOpType op, int r_lhs
       int c = GET_REG(cg);
       int x = GET_REG(cg);
       int k = GET_REG(cg);
-      int z = bind_cst(1, cg, frag);
+      int z = bind_cst(-1, cg, frag);
       OPEN_OTHER(cg, frag);
       PUSH_INSTR(frag, BytecodeStream::CALL, BytecodeProc::FUN, cg.find_builtin("int_minus"), CG::r(r_lhs), CG::r(r_rhs));
       CLOSE_AGG(cg, frag);
@@ -523,7 +523,6 @@ CG_Cond::T linear_cond(CodeGen& cg, CG_Builder& frag, BinOpType op, Mode ctx, in
       PUSH_INSTR(frag, BytecodeStream::POP, CG::r(c));
       PUSH_INSTR(frag, BytecodeStream::SIMPLIFY_LIN, CG::r(c), CG::r(c), CG::r(x), CG::r(k));
       PUSH_INSTR(frag, BytecodeStream::SUBI, CG::r(z), CG::r(k), CG::r(k));
-      PUSH_INSTR(frag, BytecodeStream::CALL, ctx, cg.find_builtin("int_lin_le"), CG::r(c), CG::r(x), CG::r(k));
       return CG_Cond::call(cg.find_builtin("int_lin_le"), ctx, CG::r(c), CG::r(x), CG::r(k));
     }
     case BOT_LE:
@@ -531,14 +530,13 @@ CG_Cond::T linear_cond(CodeGen& cg, CG_Builder& frag, BinOpType op, Mode ctx, in
       int c = GET_REG(cg);
       int x = GET_REG(cg);
       int k = GET_REG(cg);
-      int z = bind_cst(1, cg, frag);
+      int z = bind_cst(-1, cg, frag);
       OPEN_OTHER(cg, frag);
       PUSH_INSTR(frag, BytecodeStream::CALL, BytecodeProc::FUN, cg.find_builtin("int_minus"), CG::r(r_lhs), CG::r(r_rhs));
       CLOSE_AGG(cg, frag);
       PUSH_INSTR(frag, BytecodeStream::POP, CG::r(c));
       PUSH_INSTR(frag, BytecodeStream::SIMPLIFY_LIN, CG::r(c), CG::r(c), CG::r(x), CG::r(k));
       PUSH_INSTR(frag, BytecodeStream::SUBI, CG::r(z), CG::r(k), CG::r(k));
-      PUSH_INSTR(frag, BytecodeStream::CALL, ctx, cg.find_builtin("int_lin_le"), CG::r(c), CG::r(x), CG::r(k));
       return CG_Cond::call(cg.find_builtin("int_lin_le"), ctx, CG::r(c), CG::r(x), CG::r(k));
     }
     case BOT_NQ:
