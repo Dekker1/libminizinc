df61f7aaad211290efeb1d7c0d8c1c07db8d476f
Jip J. Dekker <jip@dekker.one>
2020-03-19 14:55:57 +1100

    Always add to front of defs to ensure topological order

--- include/minizinc/bytecode.hh
+++ include/minizinc/bytecode.hh
@@ -608,7 +608,7 @@ namespace MiniZinc {
       if (!_defs) {
         _defs = Definition::a(interpreter,IntVal(0),false,0,0,{},-1); // Empty Head
       }
-      _defs->appendBefore(interpreter, defs);
+      _defs->next()->appendBefore(interpreter, defs);
     }
     static Definition* a(Interpreter* interpreter,Val domain,bool binding,int pred,char mode,const std::vector<Val>& args,int ident,Val ann=IntVal(0)) {
       Definition* d = static_cast<Definition*>(::malloc(sizeof(Definition)+sizeof(Val)*(std::max(0,static_cast<int>(args.size())-1))));
