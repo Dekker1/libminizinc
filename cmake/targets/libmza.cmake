07511b31b2505c1a0b9ceeec950371a300a5d11c
Jip J. Dekker <jip@dekker.one>
2020-04-30 09:43:52 +1000

    Only build mza if gecode is found

--- cmake/targets/libmza.cmake
+++ cmake/targets/libmza.cmake
@@ -1,6 +1,8 @@
-add_library(mza SHARED
-  lib/c_interface.cpp
-  include/minizinc/c_interface.h
-)
+if(GECODE_FOUND AND USE_GECODE)
+  add_library(mza SHARED
+    lib/c_interface.cpp
+    include/minizinc/c_interface.h
+  )
 
-target_link_libraries(mza mzn)
+  target_link_libraries(mza mzn)
+endif()
