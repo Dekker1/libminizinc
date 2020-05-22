1cbac89f5c22dd794cd00ea1dfa5adca500dd750
Jip J. Dekker <jip@dekker.one>
2020-05-23 09:37:47 +1000

    Fix RET debug output

--- lib/bytecode.cpp
+++ lib/bytecode.cpp
@@ -1650,7 +1650,11 @@ namespace MiniZinc {
           break;
         case BytecodeStream::RET:
         {
-          DBG_INTERPRETER("RET %--" << std::get<0>(frame->cse_info.back()) << "(" << _procs[std::get<0>(frame->cse_info.back())].name << ")" << BytecodeProc::mode_to_string[std::get<1>(frame->cse_info.back())] << "\n");
+          DBG_INTERPRETER("RET");
+          if (!frame->cse_info.empty()) {
+            DBG_INTERPRETER(" %-- " << std::get<0>(frame->cse_info.back()) << " (" << _procs[std::get<0>(frame->cse_info.back())].name << ") " << BytecodeProc::mode_to_string[std::get<1>(frame->cse_info.back())]);
+          }
+          DBG_INTERPRETER("\n");
           assert(!_stack.empty());
 execute_ret:
           if (_stack.size()==1) {
