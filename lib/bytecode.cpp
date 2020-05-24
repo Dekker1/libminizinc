7b74e809f3e3e2de1cdb4c71c8289da88ec9b658
Guido Tack <guido.tack@monash.edu>
2020-05-24 19:56:15 +1000

    Follow alias chain in CSE

--- lib/bytecode.cpp
+++ lib/bytecode.cpp
@@ -835,7 +835,7 @@ namespace MiniZinc {
       // We are replacing another entry within the CSE table.
       assert(it->first == key && it->second.first != mode);
       it->first.destroy();
-      Val& oldVal = it->second.second;
+      Val oldVal = Val::follow_alias(it->second.second);
       BytecodeProc::Mode& oldMode = it->second.first;
       if (mode == BytecodeProc::ROOT || mode == BytecodeProc::ROOT_NEG) {
         if (oldVal.isVar()) {
